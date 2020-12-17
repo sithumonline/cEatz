1221a9d1c323e7f231bab9446305bdffb47e613d
ebonynon <sithumsandeepap@gmail.com>
2020-12-17 22:50:39 +0530

    Set loading GIF

--- src/pages/CustomersPage/CustomersPage.tsx
+++ src/pages/CustomersPage/CustomersPage.tsx
@@ -1,37 +1,38 @@
 import axios from "axios";
-import { Box, Flex } from "@chakra-ui/react";
-import { useQuery } from "react-query";
-import { environment } from "../../configs/environment";
+import {Box, Flex} from "@chakra-ui/react";
+import {useQuery} from "react-query";
+import loadingGif from "../../assets/img/Cube-1s-200px.gif";
+import {environment} from "../../configs/environment";
 import TableComponent from "../../components/CustomersPage/Table";
 import AddComponent from "../../components/CustomersPage/Add";
 
 export default function CustomersPage() {
-  const { isLoading, error, data } = useQuery("customers", () =>
-    axios.get(environment.baseURL + "/customers/").then((res) => res.data)
-  );
+    const {isLoading, error, data} = useQuery("customers", () =>
+        axios.get(environment.baseURL + "/customers/").then((res) => res.data)
+    );
 
-  let table;
+    let table;
 
-  if (error) {
-    console.log(error);
-  }
-
-  if (!data) {
-    if (isLoading) {
-      table = <h1>Nop</h1>;
+    if (error) {
+        console.log(error);
     }
-  } else {
-    table = Object.entries(data).map(([key, value]) => (
-      <TableComponent key={key} vl={value} />
-    ));
-  }
 
-  return (
-    <Flex justify="center" align="center" p={4}>
-      <Box p={1} color="white">
-        <AddComponent />
-        {table}
-      </Box>
-    </Flex>
-  );
+    if (!data) {
+        if (isLoading) {
+            table = <img src={loadingGif} alt="Loading"/>;
+        }
+    } else {
+        table = Object.entries(data).map(([key, value]) => (
+            <TableComponent key={key} vl={value}/>
+        ));
+    }
+
+    return (
+        <Flex justify="center" align="center" p={4}>
+            <Box p={1} color="white">
+                <AddComponent/>
+                {table}
+            </Box>
+        </Flex>
+    );
 }
