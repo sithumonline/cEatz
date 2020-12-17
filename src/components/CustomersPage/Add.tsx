50bf89db7fdd14f7297b0ad7f901d7285e4b629f
ebonynon <sithumsandeepap@gmail.com>
2020-12-17 21:48:03 +0530

    Customer edit/add re-fetch

--- src/components/CustomersPage/Add.tsx
+++ src/components/CustomersPage/Add.tsx
@@ -1,108 +1,112 @@
 import React from "react";
-import { useFormik } from "formik";
-import { useRef } from "react";
+import {useFormik} from "formik";
+import {useQueryCache} from "react-query";
+import {useRef} from "react";
 import {
-  Box,
-  Grid,
-  GridItem,
-  Button,
-  useDisclosure,
-  Input,
-  Stack,
-  Drawer,
-  DrawerBody,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerOverlay,
-  DrawerContent,
-  DrawerCloseButton,
+    Box,
+    Grid,
+    GridItem,
+    Button,
+    useDisclosure,
+    Input,
+    Stack,
+    Drawer,
+    DrawerBody,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,
 } from "@chakra-ui/react";
-import { AddIcon } from "@chakra-ui/icons";
-import { CustomersService } from "../../services";
-export default function AddComponent() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const btnRef: any = useRef();
-  const formik = useFormik({
-    initialValues: {
-      CusID: "",
-      FName: "",
-      LName: "",
-      Phone: "",
-      Mail: "",
-    },
+import {AddIcon} from "@chakra-ui/icons";
+import {CustomersService} from "../../services";
 
-    onSubmit: async (values, { resetForm }) => {
-      await CustomersService.AddACustomers(values);
-      resetForm({});
-      onClose();
-    },
-  });
-  return (
-    <>
-      <Box borderWidth="1px" borderRadius="lg" margin={1} w="100%" p={1}>
-        <Grid templateColumns="repeat(6, 1fr)" align="center" gap={6}>
-          <GridItem colStart={6}>
-            <Button leftIcon={<AddIcon />} colorScheme="teal" onClick={onOpen}>
-              Add Customer
-            </Button>
-          </GridItem>
-        </Grid>
-      </Box>
-      <Drawer
-        isOpen={isOpen}
-        placement="right"
-        onClose={onClose}
-        finalFocusRef={btnRef}
-      >
-        <DrawerOverlay>
-          <DrawerContent>
-            <DrawerCloseButton />
-            <DrawerHeader>Add New Customer</DrawerHeader>
-            <DrawerBody>
-              <Stack spacing={1}>
-                <Input
-                  id="CusID"
-                  placeholder="CusID"
-                  value={formik.values.CusID}
-                  onChange={formik.handleChange}
-                />
-                <Input
-                  id="FName"
-                  placeholder="FName"
-                  value={formik.values.FName}
-                  onChange={formik.handleChange}
-                />
-                <Input
-                  id="LName"
-                  placeholder="LName"
-                  value={formik.values.LName}
-                  onChange={formik.handleChange}
-                />
-                <Input
-                  id="Phone"
-                  placeholder="Phone"
-                  value={formik.values.Phone}
-                  onChange={formik.handleChange}
-                />
-                <Input
-                  id="Mail"
-                  placeholder="Mail"
-                  value={formik.values.Mail}
-                  onChange={formik.handleChange}
-                />
-              </Stack>
-            </DrawerBody>
-            <DrawerFooter>
-              <Button variant="outline" mr={3} onClick={onClose}>
-                Cancel
-              </Button>
-              <Button color="blue" onClick={() => formik.handleSubmit}>
-                Add
-              </Button>
-            </DrawerFooter>
-          </DrawerContent>
-        </DrawerOverlay>
-      </Drawer>
-    </>
-  );
+export default function AddComponent() {
+    const queryClient = useQueryCache()
+    const {isOpen, onOpen, onClose} = useDisclosure();
+    const btnRef: any = useRef();
+    const formik = useFormik({
+        initialValues: {
+            CusID: "",
+            FName: "",
+            LName: "",
+            Phone: "",
+            Mail: "",
+        },
+
+        onSubmit: async (values, {resetForm}) => {
+            await CustomersService.AddACustomers(values);
+            resetForm({});
+            await queryClient.invalidateQueries('customers')
+            onClose();
+        },
+    });
+    return (
+        <>
+            <Box borderWidth="1px" borderRadius="lg" margin={1} w="100%" p={1}>
+                <Grid templateColumns="repeat(6, 1fr)" align="center" gap={6}>
+                    <GridItem colStart={6}>
+                        <Button leftIcon={<AddIcon/>} colorScheme="teal" onClick={onOpen}>
+                            Add Customer
+                        </Button>
+                    </GridItem>
+                </Grid>
+            </Box>
+            <Drawer
+                isOpen={isOpen}
+                placement="right"
+                onClose={onClose}
+                finalFocusRef={btnRef}
+            >
+                <DrawerOverlay>
+                    <DrawerContent>
+                        <DrawerCloseButton/>
+                        <DrawerHeader>Add New Customer</DrawerHeader>
+                        <DrawerBody>
+                            <Stack spacing={1}>
+                                <Input
+                                    id="CusID"
+                                    placeholder="CusID"
+                                    value={formik.values.CusID}
+                                    onChange={formik.handleChange}
+                                />
+                                <Input
+                                    id="FName"
+                                    placeholder="FName"
+                                    value={formik.values.FName}
+                                    onChange={formik.handleChange}
+                                />
+                                <Input
+                                    id="LName"
+                                    placeholder="LName"
+                                    value={formik.values.LName}
+                                    onChange={formik.handleChange}
+                                />
+                                <Input
+                                    id="Phone"
+                                    placeholder="Phone"
+                                    value={formik.values.Phone}
+                                    onChange={formik.handleChange}
+                                />
+                                <Input
+                                    id="Mail"
+                                    placeholder="Mail"
+                                    value={formik.values.Mail}
+                                    onChange={formik.handleChange}
+                                />
+                            </Stack>
+                        </DrawerBody>
+                        <DrawerFooter>
+                            <Button variant="outline" mr={3} onClick={onClose}>
+                                Cancel
+                            </Button>
+                            <Button color="blue" onClick={() => formik.handleSubmit}>
+                                Add
+                            </Button>
+                        </DrawerFooter>
+                    </DrawerContent>
+                </DrawerOverlay>
+            </Drawer>
+        </>
+    );
 }
--- src/components/CustomersPage/Edit.tsx
+++ src/components/CustomersPage/Edit.tsx
@@ -1,110 +1,114 @@
 import React from "react";
-import { useFormik } from "formik";
-import { useRef } from "react";
+import {useFormik} from "formik";
+import {useQueryCache} from "react-query";
+import {useRef} from "react";
 import {
-  Button,
-  IconButton,
-  useColorMode,
-  useDisclosure,
-  Input,
-  Stack,
-  Drawer,
-  DrawerBody,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerOverlay,
-  DrawerContent,
-  DrawerCloseButton,
+    Button,
+    IconButton,
+    useColorMode,
+    useDisclosure,
+    Input,
+    Stack,
+    Drawer,
+    DrawerBody,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,
 } from "@chakra-ui/react";
-import { EditIcon } from "@chakra-ui/icons";
-import { CustomersService } from "../../services";
-export default function EditComponent(props: { vl: any }) {
-  const gData = props.vl;
-  const { colorMode } = useColorMode();
-  const boxColor = { light: "teal.300", dark: "teal.600" };
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const btnRef: any = useRef();
-  const formik = useFormik({
-    initialValues: {
-      CusID: gData.CusID,
-      FName: gData.FName,
-      LName: gData.LName,
-      Phone: gData.Phone,
-      Mail: gData.Mail,
-    },
+import {EditIcon} from "@chakra-ui/icons";
+import {CustomersService} from "../../services";
 
-    onSubmit: async (values, { resetForm }) => {
-      await CustomersService.UpdateACustomers(gData.ID, values);
-      resetForm({
-        values: { CusID: "", FName: "", LName: "", Phone: "", Mail: "" },
-      });
-      onClose();
-    },
-  });
-  return (
-    <>
-      <IconButton
-        aria-label=""
-        rounded="full"
-        onClick={onOpen}
-        icon={<EditIcon />}
-        bg={boxColor[colorMode]}
-      />
-      <Drawer
-        isOpen={isOpen}
-        placement="right"
-        onClose={onClose}
-        finalFocusRef={btnRef}
-      >
-        <DrawerOverlay>
-          <DrawerContent>
-            <DrawerCloseButton />
-            <DrawerHeader>Edit The Customer</DrawerHeader>
-            <DrawerBody>
-              <Stack spacing={1}>
-                <Input
-                  id="CusID"
-                  placeholder="CusID"
-                  value={formik.values.CusID}
-                  onChange={formik.handleChange}
-                />
-                <Input
-                  id="FName"
-                  placeholder="FName"
-                  value={formik.values.FName}
-                  onChange={formik.handleChange}
-                />
-                <Input
-                  id="LName"
-                  placeholder="LName"
-                  value={formik.values.LName}
-                  onChange={formik.handleChange}
-                />
-                <Input
-                  id="Phone"
-                  placeholder="Phone"
-                  value={formik.values.Phone}
-                  onChange={formik.handleChange}
-                />
-                <Input
-                  id="Mail"
-                  placeholder="Mail"
-                  value={formik.values.Mail}
-                  onChange={formik.handleChange}
-                />
-              </Stack>
-            </DrawerBody>
-            <DrawerFooter>
-              <Button variant="outline" mr={3} onClick={onClose}>
-                Cancel
-              </Button>
-              <Button color="blue" onClick={() => formik.handleSubmit}>
-                Update
-              </Button>
-            </DrawerFooter>
-          </DrawerContent>
-        </DrawerOverlay>
-      </Drawer>
-    </>
-  );
+export default function EditComponent(props: { vl: any }) {
+    const gData = props.vl;
+    const queryClient = useQueryCache()
+    const {colorMode} = useColorMode();
+    const boxColor = {light: "teal.300", dark: "teal.600"};
+    const {isOpen, onOpen, onClose} = useDisclosure();
+    const btnRef: any = useRef();
+    const formik = useFormik({
+        initialValues: {
+            CusID: gData.CusID,
+            FName: gData.FName,
+            LName: gData.LName,
+            Phone: gData.Phone,
+            Mail: gData.Mail,
+        },
+
+        onSubmit: async (values, {resetForm}) => {
+            await CustomersService.UpdateACustomers(gData.ID, values);
+            resetForm({
+                values: {CusID: "", FName: "", LName: "", Phone: "", Mail: ""},
+            });
+            await queryClient.invalidateQueries('customers')
+            onClose();
+        },
+    });
+    return (
+        <>
+            <IconButton
+                aria-label=""
+                rounded="full"
+                onClick={onOpen}
+                icon={<EditIcon/>}
+                bg={boxColor[colorMode]}
+            />
+            <Drawer
+                isOpen={isOpen}
+                placement="right"
+                onClose={onClose}
+                finalFocusRef={btnRef}
+            >
+                <DrawerOverlay>
+                    <DrawerContent>
+                        <DrawerCloseButton/>
+                        <DrawerHeader>Edit The Customer</DrawerHeader>
+                        <DrawerBody>
+                            <Stack spacing={1}>
+                                <Input
+                                    id="CusID"
+                                    placeholder="CusID"
+                                    value={formik.values.CusID}
+                                    onChange={formik.handleChange}
+                                />
+                                <Input
+                                    id="FName"
+                                    placeholder="FName"
+                                    value={formik.values.FName}
+                                    onChange={formik.handleChange}
+                                />
+                                <Input
+                                    id="LName"
+                                    placeholder="LName"
+                                    value={formik.values.LName}
+                                    onChange={formik.handleChange}
+                                />
+                                <Input
+                                    id="Phone"
+                                    placeholder="Phone"
+                                    value={formik.values.Phone}
+                                    onChange={formik.handleChange}
+                                />
+                                <Input
+                                    id="Mail"
+                                    placeholder="Mail"
+                                    value={formik.values.Mail}
+                                    onChange={formik.handleChange}
+                                />
+                            </Stack>
+                        </DrawerBody>
+                        <DrawerFooter>
+                            <Button variant="outline" mr={3} onClick={onClose}>
+                                Cancel
+                            </Button>
+                            <Button color="blue" onClick={() => formik.handleSubmit}>
+                                Update
+                            </Button>
+                        </DrawerFooter>
+                    </DrawerContent>
+                </DrawerOverlay>
+            </Drawer>
+        </>
+    );
 }
