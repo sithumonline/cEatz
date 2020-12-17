165fd9887581eea96109a331c26b6b0de59d98c1
ebonynon <sithumsandeepap@gmail.com>
2020-12-17 23:35:48 +0530

    Set confirmation box to delete option

--- src/components/CustomersPage/Table.tsx
+++ src/components/CustomersPage/Table.tsx
@@ -1,55 +1,58 @@
-import { Box, Grid, useColorMode, IconButton } from "@chakra-ui/react";
-import { DeleteIcon } from "@chakra-ui/icons";
+import {Box, Grid, useColorMode, IconButton} from "@chakra-ui/react";
+import {DeleteIcon} from "@chakra-ui/icons";
 import EditComponent from "./Edit";
-import { CustomersService } from "../../services";
+import {CustomersService} from "../../services";
+
 export default function TableComponent(props: { vl: any }) {
-  const value = props.vl;
-  const { colorMode } = useColorMode();
-  const bgColor = { light: "gray.200", dark: "gray.500" };
-  const boxColor = { light: "teal.300", dark: "teal.600" };
-  const textColor = { light: "black", dark: "gray.100" };
-  const onDelete = () => {
-    CustomersService.DeleteACustomers(value.ID);
-  };
-  return (
-    <Box
-      borderWidth="1px"
-      borderRadius="lg"
-      margin={1}
-      w="100%"
-      p={1}
-      bg={bgColor[colorMode]}
-      color={textColor[colorMode]}
-    >
-      <Grid templateColumns="repeat(7, 1fr)" gap={6}>
-        <Box borderRadius="lg" p="1" bg={boxColor[colorMode]}>
-          {value.CusID}
+    const value = props.vl;
+    const {colorMode} = useColorMode();
+    const bgColor = {light: "gray.200", dark: "gray.500"};
+    const boxColor = {light: "teal.300", dark: "teal.600"};
+    const textColor = {light: "black", dark: "gray.100"};
+    const onDelete = () => {
+        if (window.confirm("Are you sure?")) {
+            CustomersService.DeleteACustomers(value.ID);
+        }
+    };
+    return (
+        <Box
+            borderWidth="1px"
+            borderRadius="lg"
+            margin={1}
+            w="100%"
+            p={1}
+            bg={bgColor[colorMode]}
+            color={textColor[colorMode]}
+        >
+            <Grid templateColumns="repeat(7, 1fr)" gap={6}>
+                <Box borderRadius="lg" p="1" bg={boxColor[colorMode]}>
+                    {value.CusID}
+                </Box>
+                <Box borderRadius="lg" p="1" bg={boxColor[colorMode]}>
+                    {value.FName}
+                </Box>
+                <Box borderRadius="lg" p="1" bg={boxColor[colorMode]}>
+                    {value.LName}
+                </Box>
+                <Box borderRadius="lg" p="1" bg={boxColor[colorMode]}>
+                    {value.Phone}
+                </Box>
+                <Box borderRadius="lg" p="1" bg={boxColor[colorMode]}>
+                    {value.Mail}
+                </Box>
+                <Box align="center">
+                    <EditComponent vl={value}/>
+                </Box>
+                <Box align="center">
+                    <IconButton
+                        aria-label=""
+                        rounded="full"
+                        onClick={onDelete}
+                        icon={<DeleteIcon/>}
+                        bg={boxColor[colorMode]}
+                    />
+                </Box>
+            </Grid>
         </Box>
-        <Box borderRadius="lg" p="1" bg={boxColor[colorMode]}>
-          {value.FName}
-        </Box>
-        <Box borderRadius="lg" p="1" bg={boxColor[colorMode]}>
-          {value.LName}
-        </Box>
-        <Box borderRadius="lg" p="1" bg={boxColor[colorMode]}>
-          {value.Phone}
-        </Box>
-        <Box borderRadius="lg" p="1" bg={boxColor[colorMode]}>
-          {value.Mail}
-        </Box>
-        <Box align="center">
-          <EditComponent vl={value} />
-        </Box>
-        <Box align="center">
-          <IconButton
-            aria-label=""
-            rounded="full"
-            onClick={onDelete}
-            icon={<DeleteIcon />}
-            bg={boxColor[colorMode]}
-          />
-        </Box>
-      </Grid>
-    </Box>
-  );
+    );
 }
--- src/components/HomePage/Table.tsx
+++ src/components/HomePage/Table.tsx
@@ -1,52 +1,55 @@
-import { Box, Grid, useColorMode, IconButton } from "@chakra-ui/react";
-import { DeleteIcon } from "@chakra-ui/icons";
+import {Box, Grid, useColorMode, IconButton} from "@chakra-ui/react";
+import {DeleteIcon} from "@chakra-ui/icons";
 import EditComponent from "./Edit";
-import { RestaurantsService } from "../../services";
+import {RestaurantsService} from "../../services";
+
 export default function TableComponent(props: { vl: any }) {
-  const value = props.vl;
-  const { colorMode } = useColorMode();
-  const bgColor = { light: "gray.200", dark: "gray.500" };
-  const boxColor = { light: "teal.300", dark: "teal.600" };
-  const textColor = { light: "black", dark: "gray.100" };
-  const onDelete = () => {
-    RestaurantsService.DeleteARestaurant(value.ID);
-  };
-  return (
-    <Box
-      borderWidth="1px"
-      borderRadius="lg"
-      margin={1}
-      w="100%"
-      p={1}
-      bg={bgColor[colorMode]}
-      color={textColor[colorMode]}
-    >
-      <Grid templateColumns="repeat(6, 1fr)" gap={6}>
-        <Box borderRadius="lg" p="1" bg={boxColor[colorMode]}>
-          {value.ResID}
+    const value = props.vl;
+    const {colorMode} = useColorMode();
+    const bgColor = {light: "gray.200", dark: "gray.500"};
+    const boxColor = {light: "teal.300", dark: "teal.600"};
+    const textColor = {light: "black", dark: "gray.100"};
+    const onDelete = () => {
+        if (window.confirm("Are you sure?")) {
+            RestaurantsService.DeleteARestaurant(value.ID);
+        }
+    };
+    return (
+        <Box
+            borderWidth="1px"
+            borderRadius="lg"
+            margin={1}
+            w="100%"
+            p={1}
+            bg={bgColor[colorMode]}
+            color={textColor[colorMode]}
+        >
+            <Grid templateColumns="repeat(6, 1fr)" gap={6}>
+                <Box borderRadius="lg" p="1" bg={boxColor[colorMode]}>
+                    {value.ResID}
+                </Box>
+                <Box borderRadius="lg" p="1" bg={boxColor[colorMode]}>
+                    {value.Name}
+                </Box>
+                <Box borderRadius="lg" p="1" bg={boxColor[colorMode]}>
+                    {value.Location}
+                </Box>
+                <Box borderRadius="lg" p="1" bg={boxColor[colorMode]}>
+                    {value.Phone}
+                </Box>
+                <Box align="center">
+                    <EditComponent vl={value}/>
+                </Box>
+                <Box align="center">
+                    <IconButton
+                        aria-label=""
+                        rounded="full"
+                        onClick={onDelete}
+                        icon={<DeleteIcon/>}
+                        bg={boxColor[colorMode]}
+                    />
+                </Box>
+            </Grid>
         </Box>
-        <Box borderRadius="lg" p="1" bg={boxColor[colorMode]}>
-          {value.Name}
-        </Box>
-        <Box borderRadius="lg" p="1" bg={boxColor[colorMode]}>
-          {value.Location}
-        </Box>
-        <Box borderRadius="lg" p="1" bg={boxColor[colorMode]}>
-          {value.Phone}
-        </Box>
-        <Box align="center">
-          <EditComponent vl={value} />
-        </Box>
-        <Box align="center">
-          <IconButton
-            aria-label=""
-            rounded="full"
-            onClick={onDelete}
-            icon={<DeleteIcon />}
-            bg={boxColor[colorMode]}
-          />
-        </Box>
-      </Grid>
-    </Box>
-  );
+    );
 }
