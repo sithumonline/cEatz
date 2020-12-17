6b42b96ed8c564b9d1ff8aa0119a8408e75b3ed4
ebonynon <sithumsandeepap@gmail.com>
2020-12-17 22:03:03 +0530

    Restaurants edit/add re-fetch

--- src/components/HomePage/Add.tsx
+++ src/components/HomePage/Add.tsx
@@ -1,108 +1,112 @@
 import React from "react";
-import { useFormik } from "formik";
-import { useRef } from "react";
+import {useFormik} from "formik";
+import {useQueryCache} from "react-query";
+import {useRef} from "react";
 import {
-  Box,
-  Grid,
-  GridItem,
-  Button,
-  useDisclosure,
-  Input,
-  Stack,
-  Drawer,
-  DrawerBody,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerOverlay,
-  DrawerContent,
-  DrawerCloseButton,
+    Box,
+    Grid,
+    GridItem,
+    Button,
+    useDisclosure,
+    Input,
+    Stack,
+    Drawer,
+    DrawerBody,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,
 } from "@chakra-ui/react";
-import { AddIcon } from "@chakra-ui/icons";
-import { RestaurantsService } from "../../services";
-export default function AddComponent() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const btnRef: any = useRef();
-  const formik = useFormik({
-    initialValues: {
-      ResID: "",
-      Name: "",
-      Phone: "",
-      Location: "",
-      ImgURL: "",
-    },
+import {AddIcon} from "@chakra-ui/icons";
+import {RestaurantsService} from "../../services";
 
-    onSubmit: async (values, { resetForm }) => {
-      await RestaurantsService.AddARestaurant(values);
-      resetForm({});
-      onClose();
-    },
-  });
-  return (
-    <>
-      <Box borderWidth="1px" borderRadius="lg" margin={1} w="100%" p={1}>
-        <Grid templateColumns="repeat(6, 1fr)" align="center" gap={6}>
-          <GridItem colStart={6}>
-            <Button leftIcon={<AddIcon />} colorScheme="teal" onClick={onOpen}>
-              Add Restaurant
-            </Button>
-          </GridItem>
-        </Grid>
-      </Box>
-      <Drawer
-        isOpen={isOpen}
-        placement="right"
-        onClose={onClose}
-        finalFocusRef={btnRef}
-      >
-        <DrawerOverlay>
-          <DrawerContent>
-            <DrawerCloseButton />
-            <DrawerHeader>Add New Restaurant</DrawerHeader>
-            <DrawerBody>
-              <Stack spacing={1}>
-                <Input
-                  id="ResID"
-                  placeholder="ResID"
-                  value={formik.values.ResID}
-                  onChange={formik.handleChange}
-                />
-                <Input
-                  id="Name"
-                  placeholder="Name"
-                  value={formik.values.Name}
-                  onChange={formik.handleChange}
-                />
-                <Input
-                  id="Phone"
-                  placeholder="Phone"
-                  value={formik.values.Phone}
-                  onChange={formik.handleChange}
-                />
-                <Input
-                  id="Location"
-                  placeholder="Location"
-                  value={formik.values.Location}
-                  onChange={formik.handleChange}
-                />
-                <Input
-                  id="ImgURL"
-                  placeholder="ImgURL"
-                  value={formik.values.ImgURL}
-                  onChange={formik.handleChange}
-                />
-              </Stack>
-            </DrawerBody>
-            <DrawerFooter>
-              <Button variant="outline" mr={3} onClick={onClose}>
-                Cancel
-              </Button>
-              <Button color="blue" onClick={() => formik.handleSubmit}>
-                Add
-              </Button>
-            </DrawerFooter>
-          </DrawerContent>
-        </DrawerOverlay>
-      </Drawer>
-    </>
-  );
+export default function AddComponent() {
+    const queryClient = useQueryCache()
+    const {isOpen, onOpen, onClose} = useDisclosure();
+    const btnRef: any = useRef();
+    const formik = useFormik({
+        initialValues: {
+            ResID: "",
+            Name: "",
+            Phone: "",
+            Location: "",
+            ImgURL: "",
+        },
+
+        onSubmit: async (values, {resetForm}) => {
+            await RestaurantsService.AddARestaurant(values);
+            resetForm({});
+            await queryClient.invalidateQueries('restaurants')
+            onClose();
+        },
+    });
+    return (
+        <>
+            <Box borderWidth="1px" borderRadius="lg" margin={1} w="100%" p={1}>
+                <Grid templateColumns="repeat(6, 1fr)" align="center" gap={6}>
+                    <GridItem colStart={6}>
+                        <Button leftIcon={<AddIcon/>} colorScheme="teal" onClick={onOpen}>
+                            Add Restaurant
+                        </Button>
+                    </GridItem>
+                </Grid>
+            </Box>
+            <Drawer
+                isOpen={isOpen}
+                placement="right"
+                onClose={onClose}
+                finalFocusRef={btnRef}
+            >
+                <DrawerOverlay>
+                    <DrawerContent>
+                        <DrawerCloseButton/>
+                        <DrawerHeader>Add New Restaurant</DrawerHeader>
+                        <DrawerBody>
+                            <Stack spacing={1}>
+                                <Input
+                                    id="ResID"
+                                    placeholder="ResID"
+                                    value={formik.values.ResID}
+                                    onChange={formik.handleChange}
+                                />
+                                <Input
+                                    id="Name"
+                                    placeholder="Name"
+                                    value={formik.values.Name}
+                                    onChange={formik.handleChange}
+                                />
+                                <Input
+                                    id="Phone"
+                                    placeholder="Phone"
+                                    value={formik.values.Phone}
+                                    onChange={formik.handleChange}
+                                />
+                                <Input
+                                    id="Location"
+                                    placeholder="Location"
+                                    value={formik.values.Location}
+                                    onChange={formik.handleChange}
+                                />
+                                <Input
+                                    id="ImgURL"
+                                    placeholder="ImgURL"
+                                    value={formik.values.ImgURL}
+                                    onChange={formik.handleChange}
+                                />
+                            </Stack>
+                        </DrawerBody>
+                        <DrawerFooter>
+                            <Button variant="outline" mr={3} onClick={onClose}>
+                                Cancel
+                            </Button>
+                            <Button color="blue" onClick={() => formik.handleSubmit}>
+                                Add
+                            </Button>
+                        </DrawerFooter>
+                    </DrawerContent>
+                </DrawerOverlay>
+            </Drawer>
+        </>
+    );
 }
--- src/components/HomePage/Edit.tsx
+++ src/components/HomePage/Edit.tsx
@@ -1,110 +1,114 @@
 import React from "react";
-import { useFormik } from "formik";
-import { useRef } from "react";
+import {useFormik} from "formik";
+import {useQueryCache} from "react-query";
+import {useRef} from "react";
 import {
-  Button,
-  IconButton,
-  useColorMode,
-  useDisclosure,
-  Input,
-  Stack,
-  Drawer,
-  DrawerBody,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerOverlay,
-  DrawerContent,
-  DrawerCloseButton,
+    Button,
+    IconButton,
+    useColorMode,
+    useDisclosure,
+    Input,
+    Stack,
+    Drawer,
+    DrawerBody,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,
 } from "@chakra-ui/react";
-import { EditIcon } from "@chakra-ui/icons";
-import { RestaurantsService } from "../../services";
-export default function EditComponent(props: { vl: any }) {
-  const gData = props.vl;
-  const { colorMode } = useColorMode();
-  const boxColor = { light: "teal.300", dark: "teal.600" };
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const btnRef: any = useRef();
-  const formik = useFormik({
-    initialValues: {
-      ResID: gData.ResID,
-      Name: gData.Name,
-      Phone: gData.Phone,
-      Location: gData.Location,
-      ImgURL: gData.ImgURL,
-    },
+import {EditIcon} from "@chakra-ui/icons";
+import {RestaurantsService} from "../../services";
 
-    onSubmit: async (values, { resetForm }) => {
-      await RestaurantsService.UpdateARestaurant(gData.ID, values);
-      resetForm({
-        values: { ResID: "", Name: "", Phone: "", Location: "", ImgURL: "" },
-      });
-      onClose();
-    },
-  });
-  return (
-    <>
-      <IconButton
-        aria-label=""
-        rounded="full"
-        onClick={onOpen}
-        icon={<EditIcon />}
-        bg={boxColor[colorMode]}
-      />
-      <Drawer
-        isOpen={isOpen}
-        placement="right"
-        onClose={onClose}
-        finalFocusRef={btnRef}
-      >
-        <DrawerOverlay>
-          <DrawerContent>
-            <DrawerCloseButton />
-            <DrawerHeader>Edit The Restaurant</DrawerHeader>
-            <DrawerBody>
-              <Stack spacing={1}>
-                <Input
-                  id="ResID"
-                  placeholder="ResID"
-                  value={formik.values.ResID}
-                  onChange={formik.handleChange}
-                />
-                <Input
-                  id="Name"
-                  placeholder="Name"
-                  value={formik.values.Name}
-                  onChange={formik.handleChange}
-                />
-                <Input
-                  id="Phone"
-                  placeholder="Phone"
-                  value={formik.values.Phone}
-                  onChange={formik.handleChange}
-                />
-                <Input
-                  id="Location"
-                  placeholder="Location"
-                  value={formik.values.Location}
-                  onChange={formik.handleChange}
-                />
-                <Input
-                  id="ImgURL"
-                  placeholder="ImgURL"
-                  value={formik.values.ImgURL}
-                  onChange={formik.handleChange}
-                />
-              </Stack>
-            </DrawerBody>
-            <DrawerFooter>
-              <Button variant="outline" mr={3} onClick={onClose}>
-                Cancel
-              </Button>
-              <Button color="blue" onClick={() => formik.handleSubmit}>
-                Update
-              </Button>
-            </DrawerFooter>
-          </DrawerContent>
-        </DrawerOverlay>
-      </Drawer>
-    </>
-  );
+export default function EditComponent(props: { vl: any }) {
+    const gData = props.vl;
+    const queryClient = useQueryCache()
+    const {colorMode} = useColorMode();
+    const boxColor = {light: "teal.300", dark: "teal.600"};
+    const {isOpen, onOpen, onClose} = useDisclosure();
+    const btnRef: any = useRef();
+    const formik = useFormik({
+        initialValues: {
+            ResID: gData.ResID,
+            Name: gData.Name,
+            Phone: gData.Phone,
+            Location: gData.Location,
+            ImgURL: gData.ImgURL,
+        },
+
+        onSubmit: async (values, {resetForm}) => {
+            await RestaurantsService.UpdateARestaurant(gData.ID, values);
+            resetForm({
+                values: {ResID: "", Name: "", Phone: "", Location: "", ImgURL: ""},
+            });
+            await queryClient.invalidateQueries('restaurants')
+            onClose();
+        },
+    });
+    return (
+        <>
+            <IconButton
+                aria-label=""
+                rounded="full"
+                onClick={onOpen}
+                icon={<EditIcon/>}
+                bg={boxColor[colorMode]}
+            />
+            <Drawer
+                isOpen={isOpen}
+                placement="right"
+                onClose={onClose}
+                finalFocusRef={btnRef}
+            >
+                <DrawerOverlay>
+                    <DrawerContent>
+                        <DrawerCloseButton/>
+                        <DrawerHeader>Edit The Restaurant</DrawerHeader>
+                        <DrawerBody>
+                            <Stack spacing={1}>
+                                <Input
+                                    id="ResID"
+                                    placeholder="ResID"
+                                    value={formik.values.ResID}
+                                    onChange={formik.handleChange}
+                                />
+                                <Input
+                                    id="Name"
+                                    placeholder="Name"
+                                    value={formik.values.Name}
+                                    onChange={formik.handleChange}
+                                />
+                                <Input
+                                    id="Phone"
+                                    placeholder="Phone"
+                                    value={formik.values.Phone}
+                                    onChange={formik.handleChange}
+                                />
+                                <Input
+                                    id="Location"
+                                    placeholder="Location"
+                                    value={formik.values.Location}
+                                    onChange={formik.handleChange}
+                                />
+                                <Input
+                                    id="ImgURL"
+                                    placeholder="ImgURL"
+                                    value={formik.values.ImgURL}
+                                    onChange={formik.handleChange}
+                                />
+                            </Stack>
+                        </DrawerBody>
+                        <DrawerFooter>
+                            <Button variant="outline" mr={3} onClick={onClose}>
+                                Cancel
+                            </Button>
+                            <Button color="blue" onClick={() => formik.handleSubmit}>
+                                Update
+                            </Button>
+                        </DrawerFooter>
+                    </DrawerContent>
+                </DrawerOverlay>
+            </Drawer>
+        </>
+    );
 }
