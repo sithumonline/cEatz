d82bd66676e6ce645fcda2c2c31df1010b29311c
ebonynon <sithumsandeepap@gmail.com>
2020-12-18 21:22:29 +0530

    Food Items table loading when deleting

--- src/components/FoodItemsPage/Table.tsx
+++ src/components/FoodItemsPage/Table.tsx
@@ -1,7 +1,9 @@
 import {Box, Grid, useColorMode, IconButton} from "@chakra-ui/react";
 import {DeleteIcon} from "@chakra-ui/icons";
+import {useMutation} from "react-query";
 import EditComponent from "./Edit";
 import {FoodItemsService} from "../../services";
+import loadingGif from "../../assets/img/Cube-1s-200px.gif";
 
 export default function TableComponent(props: { vl: any }) {
     const value = props.vl;
@@ -9,53 +11,60 @@ export default function TableComponent(props: { vl: any }) {
     const bgColor = {light: "gray.200", dark: "gray.500"};
     const boxColor = {light: "teal.300", dark: "teal.600"};
     const textColor = {light: "black", dark: "gray.100"};
+    const [mutate, status]: any = useMutation(FoodItemsService.DeleteAFoodItem)
     const onDelete = () => {
         if (window.confirm("Are you sure?")) {
-            FoodItemsService.DeleteAFoodItem(value.ID);
+            mutate(value.ID);
         }
     };
     return (
-        <Box
-            borderWidth="1px"
-            borderRadius="lg"
-            margin={1}
-            w="100%"
-            p={1}
-            bg={bgColor[colorMode]}
-            color={textColor[colorMode]}
-        >
-            <Grid templateColumns="repeat(6, 1fr)" gap={6}>
-                <Box borderRadius="lg" p="1" bg={boxColor[colorMode]}>
-                    {value.ItmID}
-                </Box>
-                <Box borderRadius="lg" p="1" bg={boxColor[colorMode]}>
-                    {value.ResID}
-                </Box>
-                <Box borderRadius="lg" p="1" bg={boxColor[colorMode]}>
-                    {value.Name}
-                </Box>
-                <Box borderRadius="lg" p="1" bg={boxColor[colorMode]}>
-                    {value.ItmKind}
-                </Box>
-                {/* <Box borderRadius="lg" p="1" bg={boxColor[colorMode]}>
+        <>
+            {status.isLoading ? (
+                <img src={loadingGif} alt="Loading"/>
+            ) : (
+                <Box
+                    borderWidth="1px"
+                    borderRadius="lg"
+                    margin={1}
+                    w="100%"
+                    p={1}
+                    bg={bgColor[colorMode]}
+                    color={textColor[colorMode]}
+                >
+                    <Grid templateColumns="repeat(6, 1fr)" gap={6}>
+                        <Box borderRadius="lg" p="1" bg={boxColor[colorMode]}>
+                            {value.ItmID}
+                        </Box>
+                        <Box borderRadius="lg" p="1" bg={boxColor[colorMode]}>
+                            {value.ResID}
+                        </Box>
+                        <Box borderRadius="lg" p="1" bg={boxColor[colorMode]}>
+                            {value.Name}
+                        </Box>
+                        <Box borderRadius="lg" p="1" bg={boxColor[colorMode]}>
+                            {value.ItmKind}
+                        </Box>
+                        {/* <Box borderRadius="lg" p="1" bg={boxColor[colorMode]}>
           {value.smallPrice}
         </Box>
         <Box borderRadius="lg" p="1" bg={boxColor[colorMode]}>
           {value.largePrice}
         </Box> */}
-                <Box align="center">
-                    <EditComponent vl={value}/>
+                        <Box align="center">
+                            <EditComponent vl={value}/>
+                        </Box>
+                        <Box align="center">
+                            <IconButton
+                                aria-label=""
+                                rounded="full"
+                                onClick={onDelete}
+                                icon={<DeleteIcon/>}
+                                bg={boxColor[colorMode]}
+                            />
+                        </Box>
+                    </Grid>
                 </Box>
-                <Box align="center">
-                    <IconButton
-                        aria-label=""
-                        rounded="full"
-                        onClick={onDelete}
-                        icon={<DeleteIcon/>}
-                        bg={boxColor[colorMode]}
-                    />
-                </Box>
-            </Grid>
-        </Box>
+            )}
+        </>
     );
 }
