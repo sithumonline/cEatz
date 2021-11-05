2325aa643e7bcbaedbad8d6e0ddc548514ad55a8
Sithum Bopitiya <sithumsandeepap@gmail.com>
2021-11-05 17:39:35 +0530

    Fix base

--- src/configs/environment.ts
+++ src/configs/environment.ts
@@ -1,3 +1,10 @@
+const URL = process.env.REACT_APP_BASEURL;
+let bURL = "https://stormy-depths-99062.herokuapp.com/v1";
+
+if (URL) {
+  bURL = URL;
+}
+
 export const environment = {
-  baseURL: "http://localhost:3080/v1" || "https://stormy-depths-99062.herokuapp.com/v1",
+  baseURL: bURL,
 };
